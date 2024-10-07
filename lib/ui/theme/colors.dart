94a22b9de5e0aef510237806754919fe8f362baa
hijin <hijin@hijinui-MacBookAir.local>
2024-10-07 21:22:30 +0900

    feat: added color setting

---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Orange
+  static const primaryOrange50 = Color(0xfffff8ed);
+  static const primaryOrange100 = Color(0xffffefd4);
+  static const primaryOrange200 = Color(0xffffdca9);
+  static const primaryOrange300 = Color(0xffffc272);
+  static const primaryOrange400 = Color(0xfffe9d39);
+  static const primaryOrange500 = Color(0xfffd7f12);
+  static const primaryOrange600 = Color(0xffee6308);
+
+  // Secondary Blue
+  static const secondaryBlue50 = Color(0xfff6f7f9);
+  static const secondaryBlue100 = Color(0xffecedf2);
+  static const secondaryBlue200 = Color(0xffd4d7e3);
+  static const secondaryBlue300 = Color(0xffafb4ca);
+  static const secondaryBlue400 = Color(0xff838cad);
+  static const secondaryBlue500 = Color(0xff646d93);
+  static const secondaryBlue600 = Color(0xff4f567a);
+  static const secondaryBlue700 = Color(0xff414763);
+  static const secondaryBlue800 = Color(0xff383c54);
+  static const secondaryBlue900 = Color(0xff303344);
+  static const secondaryBlue950 = Color(0xff22242f);
+
+  // Gray scale
+  static const white = Color(0xffFFFFFF);
+  static const backgroundLine50 = Color(0xffFEFEFE);
+  static const backgroundLine100 = Color(0xffFAFAFA);
+  static const backgroundLine200 = Color(0xffEFEFEF);
+  static const lineBackground50 = Color(0xffF5F5F5);
+  static const lineBackground100 = Color(0xffE4E4E4);
+  static const lineBackground200 = Color(0xffD9D9D9);
+  static const text50 = Color(0xffBDBDBD);
+  static const text100 = Color(0xff9E9E9E);
+  static const text200 = Color(0xff616161);
+  static const text300 = Color(0xff3D3D3D);
+  static const text400 = Color(0xff222222);
+
+  // Semantic
+  static const error = Color(0xffFD7563);
+
+  // Illustration-Main
+  static const red = Color(0xffFF6668);
+  static const yellow = Color(0xffFFCC31);
+  static const green = Color(0xff5DD375);
+  static const blue = Color(0xff66A3FF);
+  static const purple = Color(0xff9576F0);
+  static const pink = Color(0xffFF7CAB);
+  static const gray = Color(0xffC8C8C8);
+
+  // Illustration-Light
+  static const redLight = Color(0xffFF8586);
+  static const yellowLight = Color(0xffFFE085);
+  static const greenLight = Color(0xff94E4A5);
+  static const blueLight = Color(0xff99C2FF);
+  static const purpleLight = Color(0xffB69CFF);
+  static const pinkLight = Color(0xffFFB0CC);
+  static const grayLight = Color(0xffE6E6E6);
+}
