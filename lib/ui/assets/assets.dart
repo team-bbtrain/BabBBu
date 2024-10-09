0fa4004fede4c9b802c820085873df8fcebdfadd
drizzle96 <dlxkffldk37@gmail.com>
2024-10-09 19:07:34 +0900

    feat: add icon asset

---
+++ lib/ui/assets/assets.dart
@@ -0,0 +1,39 @@
+class AppAssets {
+  static final AppImages images = AppImages();
+  static final AppIcons icons = AppIcons();
+}
+
+class AppImages {}
+
+class AppIcons {
+  final String alarmClock = 'assets/icons/alarm_clock.svg';
+  final String arrowFilled = 'assets/icons/arrow_filled.svg';
+  final String arrowLine2 = 'assets/icons/arrow_line_2.svg';
+  final String arrowLine = 'assets/icons/arrow_line.svg';
+  final String bellActivated = 'assets/icons/bell_activated.svg';
+  final String bellDefault = 'assets/icons/bell_default.svg';
+  final String book = 'assets/icons/book.svg';
+  final String box = 'assets/icons/box.svg';
+  final String camera = 'assets/icons/camera.svg';
+  final String cart = 'assets/icons/cart.svg';
+  final String checkFilled = 'assets/icons/check_filled.svg';
+  final String checkLine = 'assets/icons/check_line.svg';
+  final String closeFilled = 'assets/icons/close_filled.svg';
+  final String closeLine = 'assets/icons/close_line.svg';
+  final String eyeOff = 'assets/icons/eye_off.svg';
+  final String eyeOn = 'assets/icons/eye_on.svg';
+  final String filter = 'assets/icons/filter.svg';
+  final String flag = 'assets/icons/flag.svg';
+  final String gpsFixed = 'assets/icons/gps_fixed.svg';
+  final String handEmoji = 'assets/icons/hand_emoji.svg';
+  final String home = 'assets/icons/home.svg';
+  final String infoCircle = 'assets/icons/info_circle.svg';
+  final String list = 'assets/icons/list.svg';
+  final String location = 'assets/icons/location.svg';
+  final String plus = 'assets/icons/plus.svg';
+  final String searchFilled = 'assets/icons/search_filled.svg';
+  final String searchLine = 'assets/icons/search_line.svg';
+  final String shop = 'assets/icons/shop.svg';
+  final String timeCircle = 'assets/icons/time_circle.svg';
+  final String user = 'assets/icons/user.svg';
+}
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:BabBBu/ui/assets/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +12,37 @@ class HomeScreen extends StatelessWidget {
         title: Text('Home'),
       ),
       body: Center(
-        child: Text('Welcome to Home Screen!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to Home Screen!'),
+            // ⭐️ 아이콘 사용법
+            SvgPicture.asset(
+              AppAssets.icons.arrowLine,
+            ),
+            // 크기 조절
+            SvgPicture.asset(
+              AppAssets.icons.handEmoji,
+              width: 50,
+              height: 50,
+            ),
+            // 색상 변경
+            SvgPicture.asset(
+              AppAssets.icons.arrowLine,
+              colorFilter: ColorFilter.mode(
+                Colors.red,
+                BlendMode.srcIn,
+              ),
+            ),
+            // 회전
+            Transform.rotate(
+              angle: 3.14,
+              child: SvgPicture.asset(
+                AppAssets.icons.arrowLine,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
