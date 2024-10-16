a4241569a34bf92a2a5970d7d95bb8069e1839cf
geongupark <ggus.park@gmail.com>
2024-10-17 00:14:38 +0900

    feat: add a search text field

--- lib/ui/screens/map_screen.dart
+++ lib/ui/screens/map_screen.dart
@@ -1,3 +1,5 @@
+import 'package:BabBBu/ui/widgets/text_field/search_text_field.dart';
+import 'package:BabBBu/ui/widgets/text_field/text_field_height.dart';
 import 'package:flutter/material.dart';
 
 class MapScreen extends StatelessWidget {
@@ -8,7 +10,21 @@ class MapScreen extends StatelessWidget {
         title: Text('Map'),
       ),
       body: Center(
-        child: Text('This is the Map Screen!'),
+        child: Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              SearchTextField(
+                controller: TextEditingController(),
+                height: TextFieldHeight.lg,
+                onSearch: (query) {
+                  print('Search query: $query');
+                },
+              ),
+              Text('This is the Map Screen!'),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/ui/widgets/text_field/text_field_height.dart
@@ -0,0 +1,5 @@
+class TextFieldHeight {
+  static const double lg = 56;
+  static const double md = 48;
+  static const double sm = 40;
+}
