d54ea6b6b8f210bcd467f9398ca06f4ce486b2c6
hijin-dong <primary_dong@naver.com>
2024-10-23 23:12:16 +0900

    feat: add chipFilter Toggle and second page

--- lib/ui/screens/guide_page/guide_page.dart
+++ lib/ui/screens/guide_page/guide_page.dart
@@ -27,6 +27,8 @@ class ServiceGuidePage extends StatefulWidget {
 }
 
 class _ServiceGuidePageState extends State<ServiceGuidePage> {
+  int _selectedPage = 0; // 현재 선택된 페이지를 나타내는 변수
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,7 +76,7 @@ class _ServiceGuidePageState extends State<ServiceGuidePage> {
                 ),
                 child: AppSelectChip(
                   chipBarSpace: 8,
-                  initialSelectedIndex: 0,
+                  initialSelectedIndex: _selectedPage,
                   appChips: [
                     AppChip(
                       text: '푸드뱅크란?',
@@ -85,6 +87,11 @@ class _ServiceGuidePageState extends State<ServiceGuidePage> {
                       properties: AppChipProperties.medium,
                     )
                   ],
+                  onSingleSelectionChanged: (int index) {
+                    setState(() {
+                      _selectedPage = index;
+                    });
+                  },
                 ),
               ),
             ),
@@ -93,143 +100,239 @@ class _ServiceGuidePageState extends State<ServiceGuidePage> {
               fit: BoxFit.cover,
               width: double.infinity,
             ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '푸드뱅크가\n무엇인가요?',
-                      style: GuidePageTextStyle.titleTextStyle,
-                    ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    Text(
-                      '푸드뱅크는 식품 및 생활용품을 기부받아\n결식아동, 독거노인 등 소외 계층에게\n지원하는 나눔제도에요.',
-                      style: GuidePageTextStyle.bodyTextStyle,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      '보건복지부 산하 공공기관인\n한국사회복지협의회에서 위탁 사업을 수행하며\n식품 등 기부 활성화에 관한 법률에 근거하여\n사회 공동체 문화 확산에 이바지하고 있어요.',
-                      style: GuidePageTextStyle.bodyTextStyle,
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        Uri url = Uri.parse(
-                            'https://www.foodbank1377.org/introduce/foodbank.do');
-                        if (!await launchUrl(url)) {
-                          throw Exception('현재 페이지에 접속할 수 없어요');
-                        }
-                      },
-                      style: TextButton.styleFrom(
-                        minimumSize: Size.zero,
-                        padding: EdgeInsets.zero,
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                      ),
-                      child: Text(
-                        '더 알아보기',
-                        style: GuidePageTextStyle.textButtonTextStyle,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 12,
-              width: double.infinity,
-              color: AppColors.line50,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 40,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '기부에\n관심이 있어요!',
-                      style: GuidePageTextStyle.titleTextStyle,
-                    ),
-                    SizedBox(
-                      height: 32,
-                    ),
-                    _getQuestionBox(
-                      context,
-                      '구체적으로 무엇을 기부할 수 있나요?',
-                      '이용자에게 안전하게 제공할 수 있는 대부분의 식품과\n생활용품을 기부받아요.\n단, 유통기한이나 신선도, 사용기한에 따라\n제약이 있을 수 있어요.',
-                      '내 물건 기부 가능한지 확인하기',
-                      ScreenName.donateLogic,
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    AppExpansionTile(
-                        title: '식품별 모집 가능 기한 및 배분 기한', content: '뭔가 내용이 있겠쥬?'),
-                    AppExpansionTile(
-                      title: '생활용품별 모집 가능 기한 및 배분 기한',
-                      subtitle: '(사용 기한이 표시된 물품에 한함)',
-                      content: '얘도 내용이 있겠쥬?',
-                    ),
-                    SizedBox(
-                      height: 52,
-                    ),
-                    _getQuestionBox(
-                      context,
-                      '뉴스에 보면 박스 단위로 기부하던데,\n낱개도 기부가 가능한가요?',
-                      '당연하죠! 작은 단위의 기부물품이 모여\n누군가의 소중한 한 끼 식사가 될 수 있답니다.',
-                    ),
-                    SizedBox(
-                      height: 52,
-                    ),
-                    _getQuestionBox(
-                      context,
-                      '어떤 혜택이 있나요?',
-                      '푸드뱅크/푸드마켓 기부 시 최대 100%까지\n세제 혜택을 받으실 수 있어요.',
-                      '자세히 보기',
-                      ScreenName.detailBenefit,
-                    ),
-                    SizedBox(
-                      height: 52,
-                    ),
-                    _getQuestionBox(
-                      context,
-                      '어디에 기부를 하면 되나요?',
-                      '어려운 이웃에게 기부 물품을 전달하는 푸드뱅크와\n저소득층이 직접 매장에 방문하여\n기부물품을 구입하는 방식의 푸드마켓이\n전국 곳곳에서 온정을 기다리고 있어요!',
-                      '내 근처 기부처 찾아보기',
-                      ScreenName.map,
-                    ),
-                    SizedBox(
-                      height: 68,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            // 선택된 페이지에 따라 다른 화면 표시
+            _selectedPage == 0 ? _buildFirstPage() : _buildSecondPage(),
           ],
         ),
       ),
+    );
+  }
+
+  // 첫 번째 페이지 내용
+  Widget _buildFirstPage() {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '푸드뱅크가\n무엇인가요?',
+                  style: GuidePageTextStyle.titleTextStyle,
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  '푸드뱅크는 식품 및 생활용품을 기부받아\n결식아동, 독거노인 등 소외 계층에게\n지원하는 나눔제도에요.',
+                  style: GuidePageTextStyle.bodyTextStyle,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  '보건복지부 산하 공공기관인\n한국사회복지협의회에서 위탁 사업을 수행하며\n식품 등 기부 활성화에 관한 법률에 근거하여\n사회 공동체 문화 확산에 이바지하고 있어요.',
+                  style: GuidePageTextStyle.bodyTextStyle,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                TextButton(
+                  onPressed: () async {
+                    Uri url = Uri.parse(
+                        'https://www.foodbank1377.org/introduce/foodbank.do');
+                    if (!await launchUrl(url)) {
+                      throw Exception('현재 페이지에 접속할 수 없어요');
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    minimumSize: Size.zero,
+                    padding: EdgeInsets.zero,
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ),
+                  child: Text(
+                    '더 알아보기',
+                    style: GuidePageTextStyle.textButtonTextStyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          height: 12,
+          width: double.infinity,
+          color: AppColors.line50,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '기부에\n관심이 있어요!',
+                  style: GuidePageTextStyle.titleTextStyle,
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                _getQuestionBox(
+                  context,
+                  '구체적으로 무엇을 기부할 수 있나요?',
+                  '이용자에게 안전하게 제공할 수 있는 대부분의 식품과\n생활용품을 기부받아요.\n단, 유통기한이나 신선도, 사용기한에 따라\n제약이 있을 수 있어요.',
+                  '내 물건 기부 가능한지 확인하기',
+                  ScreenName.donateLogic,
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                AppExpansionTile(
+                    title: '식품별 모집 가능 기한 및 배분 기한', content: '뭔가 내용이 있겠쥬?'),
+                AppExpansionTile(
+                  title: '생활용품별 모집 가능 기한 및 배분 기한',
+                  subtitle: '(사용 기한이 표시된 물품에 한함)',
+                  content: '얘도 내용이 있겠쥬?',
+                ),
+                SizedBox(
+                  height: 52,
+                ),
+                _getQuestionBox(
+                  context,
+                  '뉴스에 보면 박스 단위로 기부하던데,\n낱개도 기부가 가능한가요?',
+                  '당연하죠! 작은 단위의 기부물품이 모여\n누군가의 소중한 한 끼 식사가 될 수 있답니다.',
+                ),
+                SizedBox(
+                  height: 52,
+                ),
+                _getQuestionBox(
+                  context,
+                  '어떤 혜택이 있나요?',
+                  '푸드뱅크/푸드마켓 기부 시 최대 100%까지\n세제 혜택을 받으실 수 있어요.',
+                  '자세히 보기',
+                  ScreenName.detailBenefit,
+                ),
+                SizedBox(
+                  height: 52,
+                ),
+                _getQuestionBox(
+                  context,
+                  '어디에 기부를 하면 되나요?',
+                  '어려운 이웃에게 기부 물품을 전달하는 푸드뱅크와\n저소득층이 직접 매장에 방문하여\n기부물품을 구입하는 방식의 푸드마켓이\n전국 곳곳에서 온정을 기다리고 있어요!',
+                  '내 근처 기부처 찾아보기',
+                  ScreenName.map,
+                ),
+                SizedBox(
+                  height: 68,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // 두 번째 페이지 내용
+  Widget _buildSecondPage() {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '밥뿌는\n무엇인가요?',
+                  style: GuidePageTextStyle.titleTextStyle,
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  '밥뿌는 온기와 매듭을 상징하는 모자를 쓴 캐릭터인 동시에\n여러분이 지금 함께 하고 있는 앱이에요.',
+                  style: GuidePageTextStyle.bodyTextStyle,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  '밥뿌를 통해 온기를 전달하는 기부의 문턱이\n낮아질 수 있기를 바라요.',
+                  style: GuidePageTextStyle.bodyTextStyle,
+                ),
+                SizedBox(
+                  height: 152,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          height: 12,
+          width: double.infinity,
+          color: AppColors.line50,
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 40,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '밥뿌는\n누가 만들었나요?',
+                  style: GuidePageTextStyle.titleTextStyle,
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  '밥뿌는 아산나눔재단 도전트랙 2기 밥밥트레인 팀이 만들었어\n요. 밥밥트레인 팀은 플랫폼을 통해 이웃을 연결하여 식량 재분\n배 문제를 해결하려고 노력하고 있어요.\n그 과정에서 여러분의 도움이 필요해요.',
+                  style: GuidePageTextStyle.bodyTextStyle,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  '제안하고 싶거나 궁금한 것이 있다면 언제든지\nteam.bbtrain@gmail.com으로 문의 남겨주세요.',
+                  style: GuidePageTextStyle.bodyTextStyle,
+                ),
+                SizedBox(
+                  height: 152,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
 
 Widget _getQuestionBox(
   BuildContext context,
-  title,
+  String title,
   String content, [
   String? buttonText,
   ScreenName? destination,
@@ -292,6 +395,8 @@ Widget _getQuestionBox(
                             horizontal: 10,
                             vertical: 6,
                           ),
+                          minimumSize: Size.zero,
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(6),
                           ),
@@ -331,7 +436,7 @@ void _moveScreen(BuildContext context, ScreenName screenName) {
       Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => HomeScreen()), // TODO: 기부로직 화면으로 연결
+            builder: (context) => HomeScreen()), // TODO: 지도 화면으로 연결
       );
       break;
     case ScreenName.main:
