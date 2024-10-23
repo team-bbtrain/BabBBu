511300bd441ba505ed5f567c83cb2f243544ae9a
hijin-dong <primary_dong@naver.com>
2024-10-23 17:14:19 +0900

    feat: add expansionToggleTile to show detailed description

---
+++ lib/ui/widgets/ui/expansion_toggle.dart
@@ -0,0 +1,102 @@
+import 'package:BabBBu/ui/assets/assets.dart';
+import 'package:BabBBu/ui/theme/colors.dart';
+import 'package:BabBBu/ui/theme/fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class AppExpansionTile extends StatefulWidget {
+  final String title;
+  final String content;
+  final String? subtitle;
+
+  const AppExpansionTile({
+    super.key,
+    required this.title,
+    required this.content,
+    this.subtitle,
+  });
+
+  @override
+  _AppExpansionTileState createState() => _AppExpansionTileState();
+}
+
+class _AppExpansionTileState extends State<AppExpansionTile> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              isExpanded
+                  ? Transform.rotate(
+                      angle: 3.14,
+                      child: SizedBox(
+                        height: 24,
+                        width: 24,
+                        child: SvgPicture.asset(
+                          AppAssets.icons.arrowFilled,
+                          colorFilter: ColorFilter.mode(
+                            AppColors.secondaryBlue800,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Transform.rotate(
+                      angle: 3.14 / 2,
+                      child: SizedBox(
+                        height: 24,
+                        width: 24,
+                        child: SvgPicture.asset(
+                          AppAssets.icons.arrowFilled,
+                          colorFilter: ColorFilter.mode(
+                            AppColors.secondaryBlue800,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                    ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.title,
+                    style: AppTextStyles.body2NormalMedium.copyWith(
+                      color: AppColors.text300,
+                    ),
+                  ),
+                  if (widget.subtitle != null)
+                    Text(
+                      widget.subtitle!,
+                      style: AppTextStyles.body2NormalMedium.copyWith(
+                        color: AppColors.text100,
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+        ),
+        AnimatedSize(
+          // TODO: 안에 들어갈 내용 부분에 대한 상세 설정
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: Container(
+            height: isExpanded ? null : 0,
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: isExpanded ? Text(widget.content) : null,
+          ),
+        ),
+      ],
+    );
+  }
+}
